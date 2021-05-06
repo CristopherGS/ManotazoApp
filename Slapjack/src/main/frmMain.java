72ecaaa7466fb98bf5822f95280d8c9456f8a576
CristopherGS <guerracristofer@gmail.com>
2021-05-06 07:36:33 -0600

    class Clase

--- Slapjack/src/main/frmMain.java
+++ Slapjack/src/main/frmMain.java
@@ -9,6 +9,7 @@ import java.awt.Image;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
@@ -19,8 +20,8 @@ import javax.swing.table.DefaultTableModel;
  * @author Miguel Matul <https://github.com/MigueMat4>
  */
 public class frmMain extends javax.swing.JFrame {
-    
-    private Uno generador;
+
+    private Carta generador;
     private String[] ordenJugadores;
     private String[] ordenTiempos;
     private String[] ordenLugares;
@@ -30,6 +31,7 @@ public class frmMain extends javax.swing.JFrame {
     private Jugador player3;
     String[] columnNames = {"Puesto", "Jugador", "Tiempo"};
     DefaultTableModel model = new DefaultTableModel(columnNames, 0);
+    private static Semaphore mutex = new Semaphore(1, true); // Controla el acceso a la región crítica
 
     /**
      * Creates new form frmMain
@@ -38,39 +40,40 @@ public class frmMain extends javax.swing.JFrame {
         initComponents();
         tblResultados.setModel(model);
         lblCarta.setText("");
-        generador = new Uno();
+        generador = new Carta();
         player1 = new Jugador(1);
         player2 = new Jugador(2);
         player3 = new Jugador(3);
     }
-    
+
     public class Jugador extends Thread {
+
         private final int numero;
         private boolean intento = false;
-        
+
         public Jugador(int num) {
             numero = num;
             Image img = new ImageIcon(this.getClass().getResource("/img/Player-base.png")).getImage();
             if (numero == 1) {
-                img = img.getScaledInstance(140, 140,  java.awt.Image.SCALE_SMOOTH);
+                img = img.getScaledInstance(140, 140, java.awt.Image.SCALE_SMOOTH);
                 lblJugador1.setIcon(new ImageIcon(img));
             }
             if (numero == 2) {
                 img = new ImageIcon(this.getClass().getResource("/img/Player-base-inverted.png")).getImage();
-                img = img.getScaledInstance(140, 140,  java.awt.Image.SCALE_SMOOTH);
+                img = img.getScaledInstance(140, 140, java.awt.Image.SCALE_SMOOTH);
                 lblJugador2.setIcon(new ImageIcon(img));
             }
             if (numero == 3) {
-                img = img.getScaledInstance(140, 140,  java.awt.Image.SCALE_SMOOTH);
+                img = img.getScaledInstance(140, 140, java.awt.Image.SCALE_SMOOTH);
                 lblJugador3.setIcon(new ImageIcon(img));
             }
         }
-        
+
         @Override
-        public void run(){
+        public void run() {
             System.out.println("Jugador " + this.numero + " listo para jugar");
             System.out.println("Jugador " + this.numero + " está atento al mazo");
-            while(!intento) {
+            while (!intento) {
                 if (generador.getNumeroCarta() == 5) {
                     manotazo();
                     System.out.println("Jugador " + this.numero + " entrando a la región crítica");
@@ -78,7 +81,7 @@ public class frmMain extends javax.swing.JFrame {
                     DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
                     Date date = new Date();
                     ordenTiempos[contadorJugadores] = dateFormat.format(date);
-                    ordenLugares[contadorJugadores] = String.valueOf(contadorJugadores+1) + " lugar";
+                    ordenLugares[contadorJugadores] = String.valueOf(contadorJugadores + 1) + " lugar";
                     contadorJugadores++;
                     System.out.println("Jugador " + this.numero + " saliendo de la región crítica");
                     intento = true;
@@ -91,23 +94,27 @@ public class frmMain extends javax.swing.JFrame {
                 }
             }
         }
-        
+
         public void manotazo() {
-            Image img = new ImageIcon(this.getClass().getResource("/img/P"+String.valueOf(numero)+"-playing.png")).getImage();
-            img = img.getScaledInstance(140, 140,  java.awt.Image.SCALE_SMOOTH);
-            if (numero == 1)
+            Image img = new ImageIcon(this.getClass().getResource("/img/P" + String.valueOf(numero) + "-playing.png")).getImage();
+            img = img.getScaledInstance(140, 140, java.awt.Image.SCALE_SMOOTH);
+            if (numero == 1) {
                 lblJugador1.setIcon(new ImageIcon(img));
-            if (numero == 2)
+            }
+            if (numero == 2) {
                 lblJugador2.setIcon(new ImageIcon(img));
-            if (numero == 3)
+            }
+            if (numero == 3) {
                 lblJugador3.setIcon(new ImageIcon(img));
+            }
         }
     }
-    
-    public class Uno {
+
+    public class Carta extends Thread {
+
         private int numeroCarta;
-        
-        public Uno(){
+
+        public Carta() {
             numeroCarta = 0;
             ordenJugadores = new String[3];
             ordenTiempos = new String[3];
@@ -116,27 +123,28 @@ public class frmMain extends javax.swing.JFrame {
             // Cartas
             lblCarta.setText("");
             Image img = new ImageIcon(this.getClass().getResource("/cards/0.png")).getImage();
-            img = img.getScaledInstance(93, 138,  java.awt.Image.SCALE_SMOOTH);
+            img = img.getScaledInstance(93, 138, java.awt.Image.SCALE_SMOOTH);
             lblCarta.setIcon(new ImageIcon(img));
             // Tabla
             model = new DefaultTableModel(columnNames, 0);
         }
-        
-        public void comenzar_juego(){
+
+        @Override
+        public void run() {
             System.out.println("--------------INICIO------------------------");
             System.out.println("Preparando el juego");
             System.out.println("Barajeando...");
             System.out.println("Mazo listo para el juego");
             btnJugar.setText("Jugando...");
-            while(numeroCarta != 5) {
+            while (numeroCarta != 5) {
                 try {
                     Thread.sleep(1000);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(frmMain.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                numeroCarta = (int)(Math.random() * 10 + 0);
-                Image img = new ImageIcon(this.getClass().getResource("/cards/"+ String.valueOf(numeroCarta) +".png")).getImage();
-                img = img.getScaledInstance(93, 138,  java.awt.Image.SCALE_SMOOTH);
+                numeroCarta = (int) (Math.random() * 10 + 0);
+                Image img = new ImageIcon(this.getClass().getResource("/cards/" + String.valueOf(numeroCarta) + ".png")).getImage();
+                img = img.getScaledInstance(93, 138, java.awt.Image.SCALE_SMOOTH);
                 lblCarta.setIcon(new ImageIcon(img));
             }
             try {
@@ -145,9 +153,67 @@ public class frmMain extends javax.swing.JFrame {
             } catch (InterruptedException ex) {
                 Logger.getLogger(frmMain.class.getName()).log(Level.SEVERE, null, ex);
             }
-            for(int i=0; i<3; i++){
+            for (int i = 0; i < 3; i++) {
                 Object[] rowdata;
-                rowdata = new Object[] {ordenLugares[i], ordenJugadores[i], ordenTiempos[i]};
+                rowdata = new Object[]{ordenLugares[i], ordenJugadores[i], ordenTiempos[i]};
+                model.addRow(rowdata);
+            }
+            tblResultados.setModel(model);
+            btnJugar.setText("Jugar de nuevo");
+            btnJugar.setEnabled(true);
+            System.out.println("--------------FIN------------------------\n");
+        }
+
+        public int getNumeroCarta() {
+            return numeroCarta;
+        }
+    }
+
+    public class Uno {
+
+        private int numeroCarta;
+
+        public Uno() {
+            numeroCarta = 0;
+            ordenJugadores = new String[3];
+            ordenTiempos = new String[3];
+            ordenLugares = new String[3];
+            contadorJugadores = 0;
+            // Cartas
+            lblCarta.setText("");
+            Image img = new ImageIcon(this.getClass().getResource("/cards/0.png")).getImage();
+            img = img.getScaledInstance(93, 138, java.awt.Image.SCALE_SMOOTH);
+            lblCarta.setIcon(new ImageIcon(img));
+            // Tabla
+            model = new DefaultTableModel(columnNames, 0);
+        }
+
+        public void comenzar_juego() {
+            System.out.println("--------------INICIO------------------------");
+            System.out.println("Preparando el juego");
+            System.out.println("Barajeando...");
+            System.out.println("Mazo listo para el juego");
+            btnJugar.setText("Jugando...");
+            while (numeroCarta != 5) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(frmMain.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                numeroCarta = (int) (Math.random() * 10 + 0);
+                Image img = new ImageIcon(this.getClass().getResource("/cards/" + String.valueOf(numeroCarta) + ".png")).getImage();
+                img = img.getScaledInstance(93, 138, java.awt.Image.SCALE_SMOOTH);
+                lblCarta.setIcon(new ImageIcon(img));
+            }
+            try {
+                Thread.sleep(2000);
+                System.out.println("Calculando resultados...");
+            } catch (InterruptedException ex) {
+                Logger.getLogger(frmMain.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            for (int i = 0; i < 3; i++) {
+                Object[] rowdata;
+                rowdata = new Object[]{ordenLugares[i], ordenJugadores[i], ordenTiempos[i]};
                 model.addRow(rowdata);
             }
             tblResultados.setModel(model);
@@ -270,8 +336,8 @@ public class frmMain extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void btnJugarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnJugarActionPerformed
-        generador = new Uno();
-        generador.comenzar_juego();
+        generador = new Carta();
+        generador.start();
         player1.start();
         player2.start();
         player3.start();
